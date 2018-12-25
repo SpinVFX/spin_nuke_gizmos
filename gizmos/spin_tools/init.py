9c7bb8b80955963abe5d39d805c2675a5f09fe03
Erwan Leroy <herronelou@gmail.com>
2018-12-25 12:32:00 -0500

    Remove scandir dependency in init.py
    Revert back to os.walk as scandir is not supported in every version of nuke or OS

--- gizmos/spin_tools/init.py
+++ gizmos/spin_tools/init.py
@@ -6,14 +6,11 @@ recursively adds all subfolders to plugin path.
 # python
 import os
 
-# scandir
-import scandir
-
 # nuke
 import nuke
 
 
 dirname, filename = os.path.split(os.path.abspath(__file__))
 
-for root, dirs, files in scandir.walk(dirname):
+for root, dirs, files in os.walk(dirname):
     nuke.pluginAddPath(root)
