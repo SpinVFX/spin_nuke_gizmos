e1145d23d8044922a2299a870faad61f1163d1d6
Erwan Leroy <eleroy@spinvfx.com>
2019-12-23 17:20:59 -0500

    Fix Chromatik

--- gizmos/spin_tools/spin_tools_callbacks.py
+++ gizmos/spin_tools/spin_tools_callbacks.py
@@ -5,10 +5,18 @@ Add callbacks to be run on spin_tools.
 import nuke
 
 
+def center_chromatik():
+    try:
+        node_format = nuke.selectedNode()
+    except ValueError:
+        node_format = nuke.root()['format'].value()
+    nuke.thisNode()['center'].setValue((node_format.width() / 2, node_format.height() / 2))
+
+
 def add_callbacks():
     # Add an onUserCreate callback for Chromatik in order to have the center default to the center.
     # Workaround for the fact that nuke doesn't let us set defaults on knobs properly.
     nuke.addOnUserCreate(
-        lambda: nuke.thisNode()['center'].setValue((nuke.thisNode().width()/2, nuke.thisNode().height()/2)),
+        center_chromatik,
         nodeClass='Chromatik'
     )
